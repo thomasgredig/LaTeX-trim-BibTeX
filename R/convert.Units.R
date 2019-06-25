03f32450f1d256c0ba79c9e077114c5985cd14a6
Thomas Gredig <thomas.gredig@csulb.edu>
2019-06-24 20:20:49 -0700

    include numbers with decimals

--- R/convert.Units.R
+++ R/convert.Units.R
@@ -4,8 +4,15 @@ convert.Units <- function(filename, out.filename) {
       d = readLines(filename)
       q1 = grep('\\d+~\\w+', d)
       if(length(q1)>0) {
-        d1 =  gsub('(\\d+)~(\\w+)','\\\\SI2{\\1}{\\2}',d)
-        writeLines(d1, out.filename)
+        # find 1.2~nm
+        d1 =  gsub('(\\d*\\.\\d+)~(\\w+)','\\\\SIa{\\1}{\\2}',d)
+        # find 190~nm
+        d2 =  gsub('(\\d+)~(\\w+)','\\\\SIa{\\1}{\\2}',d1)
+        writeLines(d2, out.filename)
       }
   }
 }
+
+
+a = 'bla bla 1.77~eV and 23~nm bla bla'
+gsub('(\\d*\\.\\d+)~(\\w+)','\\\\SIa{\\1}{\\2}',a)
