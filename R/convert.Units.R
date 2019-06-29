420351010d06f15410845fc5bc67d5f127f4ac69
Thomas Gredig <thomas.gredig@csulb.edu>
2019-06-29 09:22:37 -0700

    returns number of converted lines

--- R/convert.Units.R
+++ R/convert.Units.R
@@ -1,9 +1,11 @@
 # find all units
 convert.Units <- function(filename, out.filename) {
+  num = 0
   if(file.exists(filename)) {
       d = readLines(filename)
       q1 = grep('\\d+~\\w+', d)
-      if(length(q1)>0) {
+      num = length(q1)
+      if(num>0) {
         # find 1.2~nm
         d1 =  gsub('(\\d*\\.\\d+)~(\\w+)','\\\\SIa{\\1}{\\2}',d)
         # find 190~nm
@@ -11,8 +13,9 @@ convert.Units <- function(filename, out.filename) {
         writeLines(d2, out.filename)
       }
   }
+  num
 }
 
-
-a = 'bla bla 1.77~eV and 23~nm bla bla'
-gsub('(\\d*\\.\\d+)~(\\w+)','\\\\SIa{\\1}{\\2}',a)
+# test function
+#a = 'bla bla 1.77~eV and 23~nm bla bla'
+# gsub('(\\d*\\.\\d+)~(\\w+)','\\\\SIa{\\1}{\\2}',a)
