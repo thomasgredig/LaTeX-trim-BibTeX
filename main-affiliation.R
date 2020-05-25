780293c646f78e5fadb72977bffa48f2b86d03c1
Thomas Gredig <thomas.gredig@csulb.edu>
2020-05-25 14:24:50 -0700

    add maps with bing geocode

--- main-affiliation.R
+++ main-affiliation.R
@@ -6,10 +6,14 @@
 # Date: 2020-May-24
 ######################
 
-options(geonamesUsername="enter-username") # overwrite in myConfig.R
-source('config.R') # can be overwritten by myConfig.R
+#options(geonamesUsername="enter-username") # overwrite in myConfig.R
 file.bibtex = '~/Downloads/test.bib'
-library(geonames)
+source('config.R') # can be overwritten by myConfig.R
+
+#library(geonames)
+library(ggmap)
+require(RCurl)
+library(rjson)
 # example: GNpostalCodeSearch(postalcode=32611,country="USA")
 
 
@@ -47,18 +51,53 @@ d$state = gsub('.*([A-Z]{2})\\s{1}\\d{5}.*','\\1',d$name)
 d$state[which(nchar(d$state)>2)]=''
 
 
-# now find all the latitudes and longitudes
-k1 = which(d$country=='USA')
-zips = d$zip[k1]
-d$lat = NA
-d$lng = NA
-d$city = NA
-for(zp in unique(zips)) {
-  m = GNpostalCodeSearch(postalcode=zp,country="USA")
-  k2 = which(d$zip==zp)
-  d$lat[k2] = m$lat
-  d$lng[k2] = m$lng
-  d$city[k2] = m$placeName
+
+
+# # now find all the latitudes and longitudes
+# k1 = which(d$country=='USA')
+# zips = d$zip[k1]
+# d$lat = NA
+# d$lng = NA
+# d$city = NA
+# for(zp in unique(zips)) {
+#   m = GNpostalCodeSearch(postalcode=zp,country="USA")
+#   k2 = which(d$zip==zp)
+#   d$lat[k2] = m$lat
+#   d$lng[k2] = m$lng
+#   d$city[k2] = m$placeName
+# }
+# 
+# write.csv(d, file='~/Downloads/univ.csv', row.names = FALSE)
+
+
+bGeoCode <- function(str, BingMapsKey){
+  u <- URLencode(paste0("http://dev.virtualearth.net/REST/v1/Locations?q=", str, "&maxResults=1&key=", BingMapsKey))
+  d <- getURL(u)
+  j <- fromJSON(d,simplify = FALSE) 
+  if (j$resourceSets[[1]]$estimatedTotal > 0) {
+    lat <- j$resourceSets[[1]]$resources[[1]]$point$coordinates[[1]]
+    lng <- j$resourceSets[[1]]$resources[[1]]$point$coordinates[[2]]
+  }
+  else {    
+    lat <- lng <- NA
+  }
+  c(lat,lng)
+}  
+
+d$name = levels(d$name)[d$name]
+d$lng <- d$lat <- NA
+for(j in 1:nrow(d)) {
+  print(j)
+#  mj = bGeoCode(d$name[j], api.bing)
+  d$lng[j]=mj[1]
+  d$lat[j]=mj[2]
 }
 
-write.csv(d, file='~/Downloads/univ.csv', row.names = FALSE)
+# mj = bGeoCode(d$name[1], api.bing)
+# write.csv(d,'~/Downloads/OpticalLimiting.csv')
+
+d= read.csv('~/Downloads/OpticalLimiting.csv')
+d1 = subset(d, lng< -10 & lat > 10)
+my.maps = c("terrain", "terrain-background", "satellite", "roadmap", "hybrid", "terrain", "watercolor",  "toner" )
+sbbox <- make_bbox(lon = d1$lng, lat = d1$lat, f = 0.1)
+us_map <- get_map(location = sbbox, maptype = my.maps[1], source = "osm", zoom=1)
